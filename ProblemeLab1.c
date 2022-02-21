8906071bf7908e95b45295461ca904ab2e91f279
HardChallenge <100116901+HardChallenge@users.noreply.github.com>
2022-02-21 19:14:37 +0200

    Add files via upload

---
+++ ProblemeLab1.c
@@ -0,0 +1,27 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE* fisier;
+    if(fopen_s(&fisier, "laborator1", "r") != 0)
+    {
+        printf("Acest fisier nu s-a deschis");
+    }
+    else {
+        printf("Am deschis fisierul");
+        int number, sum=0, i;
+        char theString[100];
+        while(fgets(theString, 100, fisier))
+        {
+            number=0;
+            for(i=0;i<strlen(theString)-1;i++) number=number*10+(int)theString[i]-'0';
+            sum+=number;
+        }
+
+    }
+    fclose(fisier);
+    return 0;
+}
