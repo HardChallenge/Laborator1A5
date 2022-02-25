9c9cce54aa728d11f0e90a4201c9e621ae177c44
Chichirau Razvan <chichiraurazvan@yahoo.com>
2022-02-25 18:20:48 +0200

    adaugat problema 3 si 4

--- ProblemeLab1.c
+++ ProblemeLab1.c
@@ -2,6 +2,45 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
+//problema 4
+bool isPrime(int n)
+
+{
+
+    for (int tr = 2; tr < n / 2; tr++)
+
+        if ((n % tr ) == 0)
+
+            return false;
+
+    return true;
+
+}
+
+int main()
+
+{
+
+    int n;
+
+    std::cout << "Enter a number:";
+
+    std::cin >> n;
+
+    if (isPrime(n))
+
+        std::cout << n << " is prime !";
+
+    else 
+
+        std::cout << n << " is NOT prime !";
+
+    return 0;
+
+}
+
+
 
 int main()
 { //problema 2
@@ -22,6 +61,29 @@ int main()
         }
 
     }
+
+    //problema 3
+    char string[1000], words[25][200], aux[200];
+    int i=0;
+    scanf("%s", string);
+    char* token=strtok(string, " ");
+    while (token!=NULL){
+        words[i][200]=token;
+        token=strtok(NULL, " ");
+        i++;
+    }
+    for(int k=0;k<i;k++)
+      for(int j=k+1;j<i;j++) if(strlen(words[k][200])<strlen(words[j][200])) {strcpy(aux, words[k][200]);
+         strcpy(words[k][200], words[j][200]);
+         strcpy(words[j][200], aux);
+         }
+           else if(strlen(words[k][200])==strlen(words[j][200]))  if(strcmp(words[k][200],words[j][200])>0) {strcpy(aux, words[k][200]);
+         strcpy(words[k][200], words[j][200]);
+         strcpy(words[j][200], aux);
+         }
+
+     for(int k=0;k<=i;k++) printf("%s/n", words[k][200]);
+
     
     return 0;
 }
