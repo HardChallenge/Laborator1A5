c792733a46891d47360bbc9826a4a6a6c2d899a4
Chichirau Razvan <chichiraurazvan@yahoo.com>
2022-03-07 19:41:57 +0200

    update problema 2 lab3

--- Lab3_problema2/Canvas.cpp
+++ Lab3_problema2/Canvas.cpp
@@ -18,4 +18,33 @@ Canvas::Canvas(int width, int height)
 
  }
 
+ void Canvas::FillCircle(int x, int y, int ray, char ch)
+ {
+     int i,j;
+    for(i=0;i<this->lines;i++)
+      for(j=0;j<this->columns;j++) if((i - x) * (i - x) + (j - y) * (j - y) <= ray * ray ) matrix[i][j]=ch;
+                                    else matrix[i][j]=' ';
+
+ }
+
+ void Canvas::FillRect(int left, int top, int right, int bottom, char ch)
+ {
+     int i,j;
+    for(i=0;i<this->lines;i++)
+      for(j=0;j<this->columns;j++) if(i>=left && i<=right && j>=top && j<=bottom) matrix[i][j]=ch;
+                                      else matrix[i][j]=' ';
+ }
+
+ void Canvas::DrawRect(int left, int top, int right, int bottom, char ch)
+ {
+    int i,j;
+    for(i=left;i<=right;i++) {matrix[i][top]=ch; matrix[i][bottom]=ch;}
+    for(j=top+1;j<bottom-1;j++) {matrix[left][j]=ch; matrix[right][j]=ch;}
+ }
+
+ void Canvas::SetPoint(int x, int y, char ch)
+ {
+     matrix[x][y]=ch;
+ }
+
  
