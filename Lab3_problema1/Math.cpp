e54727d8371127e3e69a5b26f9154a401fa78098
Chichirau Razvan <chichiraurazvan@yahoo.com>
2022-03-07 17:06:10 +0200

    redenumire

---
+++ Lab3_problema1/Math.cpp
@@ -0,0 +1,21 @@
+#include "Math.h"
+
+static int Add(int a,int b)
+{
+   return a+b;
+}
+
+static int Add(int a,int b,int c)
+{
+   return a+b+c;
+}
+
+static int Add(double a,double b)
+{
+    return a+b;
+}
+    
+static int Add(double a,double b,double c)
+{
+    return a+b+c;
+}
---
+++ Lab3_problema1/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"Math.h"
+using namespace std;
+int main()
+{
+    Math object;
+    int a=5, b=10, c=15;
+    double x=6, z=10;
+    cout<<object.Add(a,b);
+    
+    return 0;
+
+}
