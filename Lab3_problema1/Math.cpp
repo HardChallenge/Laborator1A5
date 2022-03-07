07a384b029fc419ac2845619aac4165fc05f63f3
Chichirau Razvan <chichiraurazvan@yahoo.com>
2022-03-07 19:11:10 +0200

    Problema 1 - lab3 rezolvata

--- Lab3_problema1/Math.cpp
+++ Lab3_problema1/Math.cpp
@@ -1,21 +1,64 @@
 #include "Math.h"
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdarg.h>
+#include<cstddef>
 
-static int Add(int a,int b)
+int Math::Add(int a,int b)
 {
    return a+b;
 }
 
-static int Add(int a,int b,int c)
+int Math::Add(int a,int b,int c)
 {
    return a+b+c;
 }
 
-static int Add(double a,double b)
+int Math::Add(double a,double b)
 {
-    return a+b;
+    return (int)a+b;
 }
     
-static int Add(double a,double b,double c)
+int Math::Add(double a,double b,double c)
 {
-    return a+b+c;
+    return (int)a+b+c;
+}
+
+
+int Math::Mul(int a,int b,int c)
+{
+    return a*b*c;
+}
+
+int Math::Mul(double a,double b)
+{
+    return (int)a*b;
+}
+
+int Math::Mul(double a,double b,double c)
+{
+  return (int)a*b*c;
+}
+
+int Math::Add(int count,...)
+{
+   int i, sum=0;
+   va_list vl;
+   va_start(vl, count);
+   for(i=0;i<count;i++) sum+=va_arg(vl, int);
+   return sum;
+} // sums up a list of integers
+
+char* Math::Add(const char * sir1, const char * sir2)
+{
+    if(sir1==nullptr or sir2==nullptr) return nullptr;
+    int size;
+    size=strlen(sir1)+strlen(sir2)+1;
+    char* name;
+    name=(char*)malloc(size);
+    memset(name, 0, size);
+    memcpy(name, sir1, strlen(sir1));
+    memcpy(name+strlen(sir1), sir2, strlen(sir2));
+    return name;
 }
--- Lab3_problema1/main.cpp
+++ Lab3_problema1/main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 int main()
 {
+   Math object;
+   int a=10, b=15, c=20;
+   double x=50.5, y=75.25, z=85.35;
+   cout<<"Suma a 2 double-uri transformata in int: "<<object.Add(x,y)<<"\n";
+   cout<<"Suma a 3 int-uri este: "<<object.Add(a,b,c)<<"\n";
+   cout<<"Inmultirea a 3 double-uri transformata in int: "<<object.Mul(x,y,z)<<"\n";
+   cout<<"Suma a mai multor numere prin functia variadica: "<<object.Add(5, 10, 10, 10, 10, 10)<<"\n";
+   cout<<"Concatenarea a 2 siruri prin alocare dinamica: "<<object.Add("Laborator 3 ", "OOP");
    
     return 0;
 
