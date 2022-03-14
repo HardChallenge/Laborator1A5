9475a4605b1776a64595ea5f64e9aa70b5b90d3a
Chichirau Razvan <chichiraurazvan@yahoo.com>
2022-03-14 19:54:38 +0200

    de revizuit

--- Lab4_problema1/Sort.h
+++ Lab4_problema1/Sort.h
@@ -9,25 +9,16 @@ class Sort
 public:
 
     Sort(int count, int min, int max);
-
     Sort(int* vector, int count);
-
     Sort(int count, ...);
-
     Sort(char* text);
-
     Sort();
 
     void InsertSort(bool ascendent=false);
-
     void QuickSort(int* vector, int st, int dr, bool ascendent=false);
-
     void BubbleSort(bool ascendent=false);
-
     void Print();
-
-    int  GetElementsCount();
-
-    int  GetElementFromIndex(int index);
+    int GetElementsCount();
+    int GetElementFromIndex(int index);
 
 };
