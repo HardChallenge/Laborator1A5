22af6173f50b87a5d5f84f9425c27aa44bf038be
Chichirau Razvan <chichiraurazvan@yahoo.com>
2022-03-18 11:44:46 +0200

    Commit final

--- Lab4_problema1/main.cpp
+++ Lab4_problema1/main.cpp
@@ -1,10 +1,20 @@
 #include "Sort.h"
+#include<iostream>
+using namespace std;
 
 int main()
 {
+    int val, *vector;
     char text[20]="10,20,30,40,50";
     Sort object(text);
+    Sort object2={10,50,100};
+    Sort object3(vector, 15);
+    object2.InsertSort();
+    val=object2.GetElementFromIndex(5);
     object.Print();
+    object2.Print();
+    object3.Print();
+    cout<<val;
     return 0;
 
 }
